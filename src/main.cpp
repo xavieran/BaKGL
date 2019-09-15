ad0a2b4a0404e3830dc00cf9a125ef3a18e425ef
Emmanuel Jacyna <xavieran.lives@gmail.com>
2019-09-15 23:11:34 +1000

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 /*
- *
- *
- *
+ * Main
  */
 
 #define LOGURU_WITH_STREAMS 1
 #include <loguru.hpp>
 
-#include <getopt.h>
+#include <boost/optional.hpp>
 
+#include <boost/archive/binary_iarchive.hpp>                                                    
+#include <boost/serialization/string.hpp>                                                       
+
+#include <fstream>   
 #include <vector>
+
+#include <getopt.h>
 
 void exit_with_help(char *argv[]) {
     fprintf(stderr, "  -s=order : Encode single frame with order given\n");
@@ -27,8 +31,18 @@ int main(int argc, char *argv[]) {
         default:
             exit_with_help(argv);
         }
+    
+    std::ifstream in("krondor.001", std::ios::binary);
+    assert(in.good());
+    boost::archive::binary_iarchive iarchive(in);
+    //std::string s;
+    double s;
+    iarchive >> s;
+    std::cout << s << '\n';
 
     LOG_F(INFO, "Done");
+
+    return 0;
 }
 
 
