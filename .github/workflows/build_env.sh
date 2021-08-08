63598966c60cd4a39b75643eedb97bfca88cb6fe
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-08-09 07:42:50 +1000

    Debug actions

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install g++-11 freeglut3-dev libsdl-dev libgl-dev libglew-dev libglm-dev libglfw3-dev
 
+echo `which g++`
+ls /usr/bin | grep gcc
