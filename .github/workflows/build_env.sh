940e3fbbc767dc1978e08e775f41c97663cf0d10
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-08-09 07:41:43 +1000

    Use G++11

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -2,5 +2,5 @@
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install g++-10 freeglut3-dev libsdl-dev libgl-dev libglew-dev libglm-dev libglfw3-dev
+sudo apt-get install g++-11 freeglut3-dev libsdl-dev libgl-dev libglew-dev libglm-dev libglfw3-dev
 
