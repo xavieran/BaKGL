68fcd9aa5563367af662095b8c78051cd7aa0342
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-08-30 17:41:13 +1000

    Fix build env

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -2,4 +2,4 @@
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install g++-11 freeglut3-dev libsdl-dev libgl-dev libglew-dev libglm-dev libglfw3-dev
+sudo apt-get install g++-11 freeglut3-dev libsdl-dev libsdlmixer-dev libgl-dev libglew-dev libglm-dev libglfw3-dev
