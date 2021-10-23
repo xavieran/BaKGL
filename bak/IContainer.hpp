0bca05fa343784d00b22121b6a621970afd5e730
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-10-24 06:37:31 +1100

    Add container type enum

--- bak/IContainer.hpp
+++ bak/IContainer.hpp
@@ -5,6 +5,20 @@
 
 namespace BAK {
 
+enum class ContainerType
+{
+    Bag           =  0x0,
+    Gravestone    =  0x2,
+    Building      =  0x3,
+    Shop          =  0x4,
+    Inn           =  0x6,
+    TimirianyaHut =  0x8, // ?
+    Combat        = 0x10,
+    Chest         = 0x16,
+    FairyChest    = 0x17,
+    EventChest    = 0x25
+};
+
 class IContainer
 {
 public:
