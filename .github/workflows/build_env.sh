9c4fea42901d7ca2d7f3cddc8d5300d0c6a84335
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-12-14 08:36:06 +1100

    Add clang build

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -2,4 +2,4 @@
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install g++-11 freeglut3-dev libsdl-dev libsdl-mixer1.2-dev libsdl-sound1.2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev
+sudo apt-get install clang13 g++-11 freeglut3-dev libsdl-dev libsdl-mixer1.2-dev libsdl-sound1.2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev
