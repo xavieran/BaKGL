e33ba85adb614958083dd2e57de013fe54d4a037
Emmanuel Jacyna <xavieran.lives@gmail.com>
2021-12-14 08:47:31 +1100

    Fix clang 13

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository ppa:savoury1/llvm-defaults-13
 
 sudo apt-get update
 
-sudo apt-get install llvm-toolchain-13 g++-11 freeglut3-dev libsdl-dev libsdl-mixer1.2-dev libsdl-sound1.2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev
+sudo apt-get install clang-13 g++-11 freeglut3-dev libsdl-dev libsdl-mixer1.2-dev libsdl-sound1.2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev
