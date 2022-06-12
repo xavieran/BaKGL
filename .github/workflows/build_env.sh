70abe3b9b84d342f5dc737027840873d1e60dd24
Emmanuel Jacyna <xavieran.lives@gmail.com>
2022-06-12 21:43:07 +1000

    Add fluidsynth to build env

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository ppa:savoury1/llvm-defaults-13
 
 sudo apt-get update
 
-sudo apt-get install clang-13 g++-11 freeglut3-dev libsdl2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev 
+sudo apt-get install clang-13 g++-11 freeglut3-dev libsdl2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev libfluidsynth-dev
