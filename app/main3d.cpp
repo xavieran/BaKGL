c13a06bc7076dbd7f938189fa5cb5ba52ee0c709
Emmanuel Jacyna <xavieran.lives@gmail.com>
2023-05-14 20:42:17 +1000

    Fix typo

--- app/main3d.cpp
+++ app/main3d.cpp
@@ -255,7 +255,7 @@ int main(int argc, char** argv)
             }
         },
         [&](const auto click)
-      {
+        {
             root.OnMouseEvent(
                 Gui::LeftMouseRelease{guiScaleInv * click});
         }
