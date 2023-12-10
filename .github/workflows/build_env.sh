2a63629fdf7ba3bc1b6aa17432ec23ca3bdee956
Emmanuel Jacyna <xavieran.lives@gmail.com>
2023-12-11 08:45:35 +1100

    Update actions to clang 17 and gcc 12

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -3,12 +3,12 @@
 # For gcc
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 #sudo add-apt-repository "deb http://ppa.launchpad.net/ubuntu-toolchain-r/ppa/ubuntu kinetic main" gcc 12 is not available in 20.04...
-sudo apt-get install g++-11
+sudo apt-get install g++-12
 
 # For clang
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh 16 all
+sudo ./llvm.sh 17 all
 
 # Other deps
 sudo apt-get install freeglut3-dev libsdl2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev libfluidsynth-dev
