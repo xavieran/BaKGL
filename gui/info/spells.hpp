9d10d6787aefe80e0c9345c1336f7a5b1d82f3e3
Emmanuel Jacyna <xavieran.lives@gmail.com>
2024-01-17 13:52:46 +1100

    Add spell list to spell screen

--- gui/info/spells.hpp
+++ gui/info/spells.hpp
@@ -6,11 +6,50 @@
 #include "gui/fontManager.hpp"
 #include "gui/IGuiManager.hpp"
 #include "gui/icons.hpp"
+#include "gui/textBox.hpp"
 
 namespace Gui {
 
+
+class SpellList : public Widget
+{
+public:
+    SpellList(
+        unsigned symbolButtonIndex,
+        glm::vec2 pos,
+        const Icons& icons)
+    :
+        Widget{
+            Graphics::DrawMode::Sprite,
+            std::get<Graphics::SpriteSheetIndex>(icons.GetButton(symbolButtonIndex)),
+            std::get<Graphics::TextureIndex>(icons.GetButton(symbolButtonIndex)),
+            Graphics::ColorMode::Texture,
+            Color::black,
+            pos,
+            std::get<glm::vec2>(icons.GetButton(symbolButtonIndex)),
+            true},
+        mSpells{
+            glm::vec2{
+                std::get<glm::vec2>(icons.GetButton(symbolButtonIndex)).x + 4,
+                4},
+            glm::vec2{240, 40}}
+    {
+        AddChildBack(&mSpells);
+    }
+
+    void AddSpells(const Font& font)
+    {
+        mSpells.SetText(font, "Bane of Black Slayers, Evil Seek, Fetters of Rime, Flamecast, Life Drain, Skyfire", false, false, true);
+    }
+
+private:
+    TextBox mSpells;
+};
+
 class SpellsScreen : public Widget
 {
+    static constexpr auto sCombatSpellButtonBaseIndex = 36;
+    static constexpr auto sWorldSpellButtonBaseIndex = 55;
 public:
     SpellsScreen(
         IGuiManager& guiManager,
@@ -35,6 +74,28 @@ public:
         mSelectedCharacter{0},
         mLogger{Logging::LogState::GetLogger("Gui::SpellsScreen")}
     {
+        const auto& [_, __, dims] = icons.GetButton(36);
+        glm::vec2 pos{4,8};
+        mSpellLists.reserve(6);
+        for (unsigned i = 0; i < 4; i++)
+        {
+            mSpellLists.emplace_back(
+                sCombatSpellButtonBaseIndex + i,
+                pos,
+                icons);
+            mSpellLists.back().AddSpells(font);
+            pos += glm::vec2{0, dims.y + 2};
+        }
+
+        for (unsigned i = 0; i < 2; i++)
+        {
+            mSpellLists.emplace_back(
+                sWorldSpellButtonBaseIndex + i,
+                pos,
+                icons);
+            mSpellLists.back().AddSpells(font);
+            pos += glm::vec2{0, dims.y + 2};
+        }
         AddChildren();
     }
 
@@ -62,13 +123,18 @@ private:
 
     void AddChildren()
     {
+        for (auto& spellList : mSpellLists)
+        {
+            AddChildBack(&spellList);
+        }
     }
-
 
     IGuiManager& mGuiManager;
     const Font& mFont;
     BAK::GameState& mGameState;
     BAK::ActiveCharIndex mSelectedCharacter;
+
+    std::vector<SpellList> mSpellLists;
 
     const Logging::Logger& mLogger;
 };
