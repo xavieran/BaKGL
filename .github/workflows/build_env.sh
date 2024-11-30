1b92072efc57d2e6983f252e06302a7b14582334
Emmanuel Jacyna <xavieran.lives@gmail.com>
2024-11-30 15:04:24 +1100

    Upgrade gcc to 14 clang to 18

--- .github/workflows/build_env.sh
+++ .github/workflows/build_env.sh
@@ -4,13 +4,12 @@ set -x
 # For gcc
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
-#sudo add-apt-repository "deb http://ppa.launchpad.net/ubuntu-toolchain-r/ppa/ubuntu kinetic main" gcc 12 is not available in 20.04...
-sudo apt-get install gcc-13 g++-13
+sudo apt-get install gcc-14 g++-14
 
 # For clang
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh 17 all
+sudo ./llvm.sh 18 all
 
 # Other deps
 sudo apt-get install freeglut3-dev libsdl2-dev libgl-dev libglew-dev libxinerama-dev libxcursor-dev libxi-dev libxrandr-dev libx11-dev libfluidsynth-dev
